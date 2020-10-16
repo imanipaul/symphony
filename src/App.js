5c92713ec45c702f83eddc33c0946072a822b01b
Imani Paul <imani.paul1@gmail.com>
2020-10-16 12:25:16 -0400

    Setup file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    Symphony
-    </div>
-  );
+  return <div className="App">Symphony</div>;
 }
 
 export default App;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,30 +1,28 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [response, setResponse] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true)
-            try {
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setResponse(json);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-                const res = await fetch(url)
-                const json = await res.json();
-                setResponse(json)
-            }
-            catch (e){
-                setError(e)
-            } finally {
-                setLoading(false)
-            }
-        }
+    fetchData();
+  }, []);
 
-        fetchData()
-    }, [])
+  return { response, loading, error };
+};
 
-    return {response, loading, error}
-}
-
-export default useFetch
+export default useFetch;
