6da5e799d0a3281d429d80f2a7e24b3ba0bf3e4b
Imani Paul <imani.paul1@gmail.com>
2020-10-16 16:09:34 -0400

    Install antd ui library

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
       Symphony
+      <Button type="primary">Test Button</Button>
       <Switch>
         <Route path="/home">
           <Home />
