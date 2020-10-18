03e99d3f4004e2fa0b274d9fd95791ef186ed0a7
Imani Paul <imani.paul1@gmail.com>
2020-10-18 13:24:11 -0400

    Add useparams and usehistory to navigate pages

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -2,41 +2,43 @@ import React, { useEffect, useState } from "react";
 import { Card, Skeleton, Tag, Image } from "antd";
 import useFetch from "../hooks/useFetch";
 import "../styles/DataCard.scss";
+import { Link } from "react-router-dom";
 
 export default function DataCard(props) {
   const { response, loading, error } = useFetch(props.url);
-  const [selectedDrink, setSelectedDrinks] = useState(null);
+  const [selectedDrink, setSelectedDrink] = useState(null);
 
   useEffect(() => {
     loading && console.log("loading...");
     error && console.log("Something went wrong...Error!");
     response && console.log("response is ", response.drinks[0]);
+    selectedDrink && console.log("selected drinks is", selectedDrink);
   }, [loading, response, error]);
 
   return (
-    <Card
-      hoverable
-      style={{ width: 250, height: 380 }}
-      loading={loading}
-      className="drink-card"
-      onClick={() =>
-        console.log("clicked drink is", response.drinks[0].idDrink)
-      }
-    >
-      <Skeleton loading={loading}>
-        {response && (
-          <>
-            <Image src={response.drinks[0].strDrinkThumb} />
+    <Link to={response ? `/drink/${response.drinks[0].idDrink}` : "/"}>
+      <Card
+        hoverable
+        style={{ width: 250, height: 380 }}
+        loading={loading}
+        className="drink-card"
+        onClick={() => setSelectedDrink(response.drinks[0].idDrink)}
+      >
+        <Skeleton loading={loading}>
+          {response && (
+            <>
+              <Image src={response.drinks[0].strDrinkThumb} />
 
-            <h3>{response.drinks[0].strDrink}</h3>
-            <Tag>{response.drinks[0].strAlcoholic}</Tag>
-            <Tag>{response.drinks[0].strGlass}</Tag>
-            {response.drinks[0].strIBA && (
-              <Tag>{response.drinks[0].strIBA}</Tag>
-            )}
-          </>
-        )}
-      </Skeleton>
-    </Card>
+              <h3>{response.drinks[0].strDrink}</h3>
+              <Tag>{response.drinks[0].strAlcoholic}</Tag>
+              <Tag>{response.drinks[0].strGlass}</Tag>
+              {response.drinks[0].strIBA && (
+                <Tag>{response.drinks[0].strIBA}</Tag>
+              )}
+            </>
+          )}
+        </Skeleton>
+      </Card>
+    </Link>
   );
 }
--- src/views/Drink.js
+++ src/views/Drink.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 export default function Drink(props) {
-  return <div>Drink Page</div>;
+  let { id } = useParams();
+  let history = useHistory();
+
+  return (
+    <div>
+      Drink Page
+      <p>This is a page for a individual drink</p>
+      <p>id is {id}</p>
+      <button type="button" onClick={() => history.push("/home")}>
+        Go Home
+      </button>
+    </div>
+  );
 }
